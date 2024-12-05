2c458f930e0c16ad20b1610d7ec6dc0eff195024
Lucacpt <luca.ginolfi@gmail.com>
2024-12-05 13:21:56 +0100

    have been added the coursepage

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import HomePage from "./homePage.jsx";
 import NegozioPunti from "./negozioPunti.jsx";
 import AreaUtente from "./areaUtente.jsx";
 import QuizApp from "./quiz.jsx";
+import CoursePage from "./coursePage.jsx";
 import SupportRequest from "./supportRequestPage.jsx";
 import AdminTicketDashboard from "./admin/adminTicketDashboard";
 import AdminTicketDetail from "./admin/adminTicketDetail.jsx";
@@ -21,13 +22,14 @@ root.render(
         <Header />
         <main className="flex-grow">
           <Routes>
-            <Route path="/" element={<HomePage />} />  // landing page
+            <Route path="/" element={<HomePage />} /> //landing page
             <Route path="/registrazione" element={<Registrazione />} />
             <Route path="/login" element={<Login />} />
             <Route path="/homepage" element={<HomePage />} />
             <Route path="/negoziopunti" element={<NegozioPunti />} />
             <Route path="/areaUtente" element={<AreaUtente />} />
             <Route path="/quiz" element={<QuizApp />} />
+            <Route path="/coursePage" element={<CoursePage />} />
             <Route path="/supportrequestpage" element={<SupportRequest />} />
             <Route path="/admin/adminTicketDashboard" element={<AdminTicketDashboard />} />
             <Route path="/admin/adminTicketDetail" element={<AdminTicketDetail />} />
