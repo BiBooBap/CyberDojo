31fdbd5d30dc075a28cc7f1ecde52b458ccea2f1
Antonio Maiorano <antomaio19@gmail.com>
2024-12-11 15:11:13 +0100

    Adjusted homepage for responsive and normal design, added buttons with user control (to continue)

--- client/src/visitor/homePage.jsx
+++ client/src/visitor/homePage.jsx
@@ -4,11 +4,11 @@ import courseFacade from "../services/courseFacade";
 const HomePage = ({ user }) => {
   const [courses, setCourses] = useState([]);
   const [error, setError] = useState(null);
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const token = localStorage.getItem("token");
         const data = await courseFacade.getAllCourses(token); //CONTROLLA SE IL TOKEN È PRESENTE (corretto). Non controlla se l'username esiste come definito nel backend
         setCourses(data);
       } catch (error) {
@@ -19,6 +19,16 @@ const HomePage = ({ user }) => {
 
     fetchCourses();
   }, []);
+
+  const introductionCourses = courses.filter(
+    (course) => course.difficulty === "Facile"
+  );
+  const intermediateCourses = courses.filter(
+    (course) => course.difficulty === "Medio"
+  );
+  const advancedCourses = courses.filter(
+    (course) => course.difficulty === "Difficile"
+  );
 
   const handleButtonClick = async (course) => {
     if (!user) {
@@ -36,39 +46,151 @@ const HomePage = ({ user }) => {
   };
 
   return (
-    <div>
-      <h1 className="font-Montserrat justify-self-center font-bold text-xl mb-3 mt-3">
+    <div className="px-8 py-4">
+      <h1 className="font-Montserrat font-bold flex lg:items-start items-center lg:justify-normal justify-center text-xl mb-3 mt-3">
         Panoramica corsi
       </h1>
-      {error ? (
-        <p className="text-red-500">{error}</p>
-      ) : (
-        courses.map((course) => (
-          <div key={course.id} className="card-body p-3 mb-3">
-            <img
-              src={course.icon}
-              alt="Foto corso"
-              className="logo w-14 h-14"
-            />
-            <h3 className="font-bold">{course.title}</h3>
-            <p className="text-xs mb-1">
-              Livello difficoltà: {course.difficulty}
-            </p>
-            <button
-              className="button-CD p-[7px]"
-              onClick={() => handleButtonClick(course)}
-            >
-              {user
-                ? course.isEnrolled
-                  ? course.isCompleted
-                    ? "Ripeti"
-                    : "Riprendi"
-                  : "Iscriviti"
-                : "Iscriviti"}
-            </button>
-          </div>
-        ))
-      )}
+      <div className="md:flex lg:flex-row justify-center lg:justify-normal space-x-3">
+        {error ? (
+          <p className="text-red-500">{error}</p>
+        ) : (
+          courses.map((course) => (
+            <div key={course.id} className="card-body p-3 mb-3">
+              <img
+                src={course.icon}
+                alt="Foto corso"
+                className="logo w-14 h-14"
+              />
+              <h3 className="font-bold">{course.title}</h3>
+              <p className="text-xs mb-1">
+                Livello difficoltà: {course.difficulty}
+              </p>
+              {user && (
+                <button
+                  className="button-CD p-[7px] text-sm"
+                  onClick={() => handleButtonClick(course)}
+                >
+                  {user
+                    ? course.isEnrolled
+                      ? course.isCompleted
+                        ? "Ripeti"
+                        : "Riprendi"
+                      : "Iscriviti"
+                    : "Iscriviti"}
+                </button>
+              )}
+            </div>
+          ))
+        )}
+      </div>
+      <h1 className="font-Montserrat font-bold flex lg:items-start items-center lg:justify-normal justify-center text-xl mb-3 mt-3">
+        Corsi introduttivi
+      </h1>
+      <div className="md:flex lg:flex-row justify-center lg:justify-normal space-x-3">
+        {error ? (
+          <p className="text-red-500">{error}</p>
+        ) : (
+          introductionCourses.map((course) => (
+            <div key={course.id} className="card-body p-3 mb-3">
+              <img
+                src={course.icon}
+                alt="Foto corso"
+                className="logo w-14 h-14"
+              />
+              <h3 className="font-bold">{course.title}</h3>
+              <p className="text-xs mb-1">
+                Livello difficoltà: {course.difficulty}
+              </p>
+              {user && (
+                <button
+                  className="button-CD p-[7px] text-sm"
+                  onClick={() => handleButtonClick(course)}
+                >
+                  {user
+                    ? course.isEnrolled
+                      ? course.isCompleted
+                        ? "Ripeti"
+                        : "Riprendi"
+                      : "Iscriviti"
+                    : "Iscriviti"}
+                </button>
+              )}
+            </div>
+          ))
+        )}
+      </div>
+      <h1 className="font-Montserrat font-bold flex lg:items-start items-center lg:justify-normal justify-center text-xl mb-3 mt-3">
+        Corsi intermedi
+      </h1>
+      <div className="md:flex lg:flex-row justify-center lg:justify-normal space-x-3">
+        {error ? (
+          <p className="text-red-500">{error}</p>
+        ) : (
+          intermediateCourses.map((course) => (
+            <div key={course.id} className="card-body p-3 mb-3">
+              <img
+                src={course.icon}
+                alt="Foto corso"
+                className="logo w-14 h-14"
+              />
+              <h3 className="font-bold">{course.title}</h3>
+              <p className="text-xs mb-1">
+                Livello difficoltà: {course.difficulty}
+              </p>
+              {user && (
+                <button
+                  className="button-CD p-[7px] text-sm"
+                  onClick={() => handleButtonClick(course)}
+                >
+                  {user
+                    ? course.isEnrolled
+                      ? course.isCompleted
+                        ? "Ripeti"
+                        : "Riprendi"
+                      : "Iscriviti"
+                    : "Iscriviti"}
+                </button>
+              )}
+            </div>
+          ))
+        )}
+      </div>
+      <h1 className="font-Montserrat font-bold flex lg:items-start items-center lg:justify-normal justify-center text-xl mb-3 mt-3">
+        Corsi avanzati
+      </h1>
+      <div className="md:flex lg:flex-row justify-center lg:justify-normal space-x-3">
+        {error ? (
+          <p className="text-red-500">{error}</p>
+        ) : (
+          advancedCourses.map((course) => (
+            <div key={course.id} className="card-body p-3 mb-3">
+              <img
+                src={course.icon}
+                alt="Foto corso"
+                className="logo w-14 h-14"
+              />
+              <h3 className="font-bold">{course.title}</h3>
+              <p className="text-xs mb-1">
+                Livello difficoltà: {course.difficulty}
+              </p>
+              {user && (
+                <button
+                  className="button-CD p-[7px] text-sm"
+                  onClick={() => handleButtonClick(course)}
+                >
+                  {user
+                    ? course.isEnrolled
+                      ? course.isCompleted
+                        ? "Ripeti"
+                        : "Riprendi"
+                      : "Iscriviti"
+                    : "Iscriviti"}
+                </button>
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
