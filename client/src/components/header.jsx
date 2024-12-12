cbb42562abefb198189b5fd2fe0f1d9daa999481
Lucacpt <luca.ginolfi@gmail.com>
2024-12-12 19:31:31 +0100

    fixed header issues. has been modified db insert. has been added the img/shop folder

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -94,7 +94,7 @@ const Header = () => {
                 <button className="nav-link hover:bg-[#4b2153] px-2 py-2 rounded text-center mx-2 font-bold text-lg">
                   <Link to="/areaUtente">
                     <img
-                      src="/img/user-icon.png"
+                      src="/img/shop/iconautente.png"
                       alt="Area Utente"
                       className="w-6 h-6 inline"
                     />
