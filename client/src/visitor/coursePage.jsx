a77cd01ce00b6d47f81c541ae779c2e547797e3b
CubeTheMaster <marco.derosa115@gmail.com>
2024-12-14 18:00:00 +0100

    Redesigned the cursePage to make it prettier, added a flip animation to the cards in the homePage, for the guest, to see the course description

--- client/src/visitor/coursePage.jsx
+++ client/src/visitor/coursePage.jsx
@@ -4,7 +4,7 @@ import courseFacade from "../services/courseFacade";
 
 function CoursePage() {
   const [searchParams] = useSearchParams();
-  const courseId = searchParams.get('corso');
+  const courseId = searchParams.get("corso");
   const [course, setCourse] = useState(null);
 
   useEffect(() => {
@@ -25,58 +25,69 @@ function CoursePage() {
   }
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow p-8 bg-white">
-        <div className="max-w-5xl mx-auto">
-          {/* Course Title Section */}
-          <div className="flex items-center justify-between mb-8">
-            <div className="flex items-center space-x-4">
-              <img src={course.icon} alt="Course Icon" className="w-32 h-32" />
-              <h1 className="text-4xl font-bold">{course.title}</h1>
-            </div>
-            <div className="text-2xl font-semibold text-purple-600">{course.description}</div>
-          </div>
-
-          {/* Progress Bar */}
-          <div className="w-full bg-gray-200 rounded-full h-4 mb-8">
-            <div
-              className="bg-green-500 h-4 rounded-full"
-              style={{ width: `${course.progress}%` }}
-            ></div>
-          </div>
-
-          {/* Final Quiz Button */}
-          <button className="bg-yellow-500 text-white py-3 px-6 rounded-lg mb-8 flex items-center space-x-2">
+    <div className="overflow-auto font-Montserrat h-screen md:inline-flex p-8 bg-white ms:flex">
+      {/* Progress Bar */}
+      <div>
+        <img src={course.icon} alt="Course Icon" className="w-72 h-72" />
+        <div className="w-72 bg-gray-200 rounded-full h-4">
+          <div
+            className="bg-green-500 h-4 rounded-full"
+            style={{ width: `${course.progress}%` }}
+          ></div>
+        </div>
+        <div className="text-2xl font-semibold text-purple-600">
+          {course.description}
+        </div>
+        <div>
+          <button
+            className="bg-yellow-500 text-white py-3 px-6 rounded-3xl space-x-2"
+            disabled={course.progress !== 100}
+          >
             <span className="font-bold">Quiz Finale</span>
             {course.progress === 100 ? (
-              <span role="img" aria-label="unlock icon">🔓</span>
+              <span role="img" aria-label="unlock icon">
+                🔓
+              </span>
             ) : (
-              <span role="img" aria-label="lock icon">🔒</span>
+              <span role="img" aria-label="lock icon">
+                🔒
+              </span>
             )}
           </button>
-
-          {/* Topics Section */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            {course.lessons.map((lesson, index) => (
-              <div
-                key={index}
-                className="bg-yellow-400 p-6 rounded-lg shadow-md flex items-start space-x-4"
-              >
-                <div>
-                  <h2 className="text-2xl font-bold mb-2">{lesson.name}</h2>
-                </div>
-                <div className="ml-auto">
-                  {lesson.completed ? (
-                    <span className="text-green-500 font-bold text-xl">✔️</span>
-                  ) : (
-                    <span className="text-gray-500 font-bold text-xl">⏳</span>
-                  )}
-                </div>
-              </div>
-            ))}
-          </div>
         </div>
-      </main>
+      </div>
+
+      {/* Course Title Section */}
+      <div>
+        <div className="flex items-center mx-8">
+          <h1 className="text-4xl font-bold">{course.title}</h1>
+        </div>
+      </div>
+
+      {/* Topics Section */}
+      <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 flow max-w-md">
+          {course.lessons.map((lesson, index) => (
+            <div
+              key={index}
+              className="bg-yellow-400 p-6 rounded-2xl shadow-md space-x-4 mb-3"
+            >
+              <div className="flex justify-end">
+                {lesson.completed ? (
+                  <span className="text-green-500 font-bold text-xl">✔️</span>
+                ) : (
+                  <span className="text-gray-500 font-bold text-xl">⏳</span>
+                )}
+              </div>
+              <div>
+                <button className="text-2xl font-bold mb-2">
+                  {lesson.name}
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/visitor/homePage.jsx
+++ client/src/visitor/homePage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import courseFacade from "../services/courseFacade";
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 const HomePage = ({ user }) => {
   const [courses, setCourses] = useState([]);
@@ -57,34 +57,35 @@ const HomePage = ({ user }) => {
     if (!user) {
       alert("È necessario registrarsi per iscriversi ai corsi.");
     } else {
-
       const button = event.target;
       const originalText = button.textContent;
 
-      if(originalText === "Riprendi" || originalText === "Ripeti") {
+      if (originalText === "Riprendi" || originalText === "Ripeti") {
         window.location.href = "/coursePage/?corso=" + course._id;
       } else {
+        button.disabled = true;
+        button.textContent = "Iscrizione in corso...";
 
-      button.disabled = true;
-      button.textContent = "Iscrizione in corso...";
-  
-      try {
-        await courseFacade.enrollCourse(course._id);
-        button.textContent = "Riprendi";
-        alert("Iscrizione avvenuta con successo!");
-        window.location.reload();
-      } catch (error) {
-        console.error("Errore durante l'iscrizione al corso:", error);
-        button.textContent = originalText;
-        alert("Errore durante l'iscrizione al corso.");
-      } finally {
-        button.disabled = false;
+        try {
+          await courseFacade.enrollCourse(course._id);
+          button.textContent = "Riprendi";
+          alert("Iscrizione avvenuta con successo!");
+          window.location.reload();
+        } catch (error) {
+          console.error("Errore durante l'iscrizione al corso:", error);
+          button.textContent = originalText;
+          alert("Errore durante l'iscrizione al corso.");
+        } finally {
+          button.disabled = false;
+        }
       }
-    }
     }
   };
 
-  console.log("User enrolled courses:", user ? user.enrolled_courses : "No user");
+  console.log(
+    "User enrolled courses:",
+    user ? user.enrolled_courses : "No user"
+  );
 
   return (
     <div className="px-8 py-4">
@@ -96,17 +97,27 @@ const HomePage = ({ user }) => {
           <p className="text-red-500">{error}</p>
         ) : (
           courses.map((course) => (
-            <div key={course._id} className="card-body p-6 mb-6 bg-white rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 flex flex-col items-center w-80">
-              <img
-                src={course.course_image}
-                alt="Foto corso"
-                className="logo w-14 h-14"
-              />
-              <h3 className="font-bold text-lg text-center mb-2">{course.name}</h3>
-              <p className="text-xs mb-4 text-center">
-                Livello difficoltà: {course.difficulty}
-              </p>
-              {(
+            <div
+              key={course._id}
+              className={`card-body relative h-72 w-72 rounded-lg shadow-lg transition-all duration-500 [transform-style:preserve-3d] ${
+                !user ? "group hover:[transform:rotateY(180deg)]" : ""
+              }`}
+            >
+              {/* Fronte della card */}
+              <div className="absolute inset-0 rounded-lg [backface-visibility:hidden] flex flex-col justify-between items-center p-6">
+                <div>
+                  <img
+                    src={course.course_image}
+                    alt="Foto corso"
+                    className="logo w-14 h-14 mb-4 justify-self-center"
+                  />
+                  <h3 className="font-bold text-lg text-center mb-2">
+                    {course.name}
+                  </h3>
+                  <p className="text-xs mb-4 text-center">
+                    Livello difficoltà: {course.difficulty}
+                  </p>
+                </div>
                 <button
                   className="button-CD px-4 py-2 bg-[#e0a11b] text-white rounded hover:bg-[#d18f1a] transition duration-200"
                   onClick={(event) => handleButtonClick(course, event)}
@@ -119,7 +130,11 @@ const HomePage = ({ user }) => {
                       : "Iscriviti"
                     : "Iscriviti"}
                 </button>
-              )}
+              </div>
+              {/* Retro della card con la descrizione del corso */}
+              <div className="card-body absolute inset-0 h-full w-full rounded-lg px-6 py-4 text-center [transform:rotateY(180deg)] [backface-visibility:hidden] flex items-center justify-center">
+                <p className="text-black">{course.description}</p>
+              </div>
             </div>
           ))
         )}
@@ -132,18 +147,27 @@ const HomePage = ({ user }) => {
           <p className="text-red-500">{error}</p>
         ) : (
           introductionCourses.map((course) => (
-            <div key={course._id}
-            className= "card-body p-6 mb-6 bg-white rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 flex flex-col items-center w-80">
-              <img
-                src={course.course_image}
-                alt="Foto corso"
-                className="logo w-14 h-14"
-              />
-              <h3 className="font-bold text-lg text-center mb-2">{course.name}</h3>
-              <p className="text-xs mb-4 text-center">
-                Livello difficoltà: {course.difficulty}
-              </p>
-              {(
+            <div
+              key={course._id}
+              className={`card-body relative h-72 w-72 rounded-lg shadow-lg transition-all duration-500 [transform-style:preserve-3d] ${
+                !user ? "group hover:[transform:rotateY(180deg)]" : ""
+              }`}
+            >
+              {/* Fronte della card */}
+              <div className="absolute inset-0 rounded-lg [backface-visibility:hidden] flex flex-col justify-between items-center p-6">
+                <div>
+                  <img
+                    src={course.course_image}
+                    alt="Foto corso"
+                    className="logo w-14 h-14 mb-4 justify-self-center"
+                  />
+                  <h3 className="font-bold text-lg text-center mb-2">
+                    {course.name}
+                  </h3>
+                  <p className="text-xs mb-4 text-center">
+                    Livello difficoltà: {course.difficulty}
+                  </p>
+                </div>
                 <button
                   className="button-CD px-4 py-2 bg-[#e0a11b] text-white rounded hover:bg-[#d18f1a] transition duration-200"
                   onClick={(event) => handleButtonClick(course, event)}
@@ -156,7 +180,11 @@ const HomePage = ({ user }) => {
                       : "Iscriviti"
                     : "Iscriviti"}
                 </button>
-              )}
+              </div>
+              {/* Retro della card con la descrizione del corso */}
+              <div className="card-body absolute inset-0 h-full w-full rounded-lg px-6 py-4 text-center [transform:rotateY(180deg)] [backface-visibility:hidden] flex items-center justify-center">
+                <p className="text-black">{course.description}</p>
+              </div>
             </div>
           ))
         )}
@@ -169,18 +197,27 @@ const HomePage = ({ user }) => {
           <p className="text-red-500">{error}</p>
         ) : (
           intermediateCourses.map((course) => (
-            <div key={course._id} 
-            className="card-body p-6 mb-6 bg-white rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 flex flex-col items-center w-80">
-              <img
-                src={course.course_image}
-                alt="Foto corso"
-                className="logo w-14 h-14"
-              />
-              <h3 className="font-bold text-lg text-center mb-2">{course.name}</h3>
-              <p className="text-xs mb-4 text-center">
-                Livello difficoltà: {course.difficulty}
-              </p>
-              {(
+            <div
+              key={course._id}
+              className={`card-body relative h-72 w-72 rounded-lg shadow-lg transition-all duration-500 [transform-style:preserve-3d] ${
+                !user ? "group hover:[transform:rotateY(180deg)]" : ""
+              }`}
+            >
+              {/* Fronte della card */}
+              <div className="absolute inset-0 rounded-lg [backface-visibility:hidden] flex flex-col justify-between items-center p-6">
+                <div>
+                  <img
+                    src={course.course_image}
+                    alt="Foto corso"
+                    className="logo w-14 h-14 mb-4 justify-self-center"
+                  />
+                  <h3 className="font-bold text-lg text-center mb-2">
+                    {course.name}
+                  </h3>
+                  <p className="text-xs mb-4 text-center">
+                    Livello difficoltà: {course.difficulty}
+                  </p>
+                </div>
                 <button
                   className="button-CD px-4 py-2 bg-[#e0a11b] text-white rounded hover:bg-[#d18f1a] transition duration-200"
                   onClick={(event) => handleButtonClick(course, event)}
@@ -193,7 +230,11 @@ const HomePage = ({ user }) => {
                       : "Iscriviti"
                     : "Iscriviti"}
                 </button>
-              )}
+              </div>
+              {/* Retro della card con la descrizione del corso */}
+              <div className="card-body absolute inset-0 h-full w-full rounded-lg px-6 py-4 text-center [transform:rotateY(180deg)] [backface-visibility:hidden] flex items-center justify-center">
+                <p className="text-black">{course.description}</p>
+              </div>
             </div>
           ))
         )}
@@ -206,18 +247,27 @@ const HomePage = ({ user }) => {
           <p className="text-red-500">{error}</p>
         ) : (
           advancedCourses.map((course) => (
-            <div key={course._id} 
-            className="card-body p-6 mb-6 bg-white rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 flex flex-col items-center w-80">
-              <img
-                src={course.course_image}
-                alt="Foto corso"
-                className="logo w-14 h-14"
-              />
-              <h3 className="font-bold text-lg text-center mb-2">{course.name}</h3>
-              <p className="text-xs mb-4 text-center">
-                Livello difficoltà: {course.difficulty}
-              </p>
-              {(
+            <div
+              key={course._id}
+              className={`card-body relative h-72 w-72 rounded-lg shadow-lg transition-all duration-500 [transform-style:preserve-3d] ${
+                !user ? "group hover:[transform:rotateY(180deg)]" : ""
+              }`}
+            >
+              {/* Fronte della card */}
+              <div className="absolute inset-0 rounded-lg [backface-visibility:hidden] flex flex-col justify-between items-center p-6">
+                <div>
+                  <img
+                    src={course.course_image}
+                    alt="Foto corso"
+                    className="logo w-14 h-14 mb-4 justify-self-center"
+                  />
+                  <h3 className="font-bold text-lg text-center mb-2">
+                    {course.name}
+                  </h3>
+                  <p className="text-xs mb-4 text-center">
+                    Livello difficoltà: {course.difficulty}
+                  </p>
+                </div>
                 <button
                   className="button-CD px-4 py-2 bg-[#e0a11b] text-white rounded hover:bg-[#d18f1a] transition duration-200"
                   onClick={(event) => handleButtonClick(course, event)}
@@ -230,7 +280,11 @@ const HomePage = ({ user }) => {
                       : "Iscriviti"
                     : "Iscriviti"}
                 </button>
-              )}
+              </div>
+              {/* Retro della card con la descrizione del corso */}
+              <div className="card-body absolute inset-0 h-full w-full rounded-lg px-6 py-4 text-center [transform:rotateY(180deg)] [backface-visibility:hidden] flex items-center justify-center">
+                <p className="text-black">{course.description}</p>
+              </div>
             </div>
           ))
         )}
